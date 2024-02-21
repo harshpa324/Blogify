04138fb0700f219b93f77e40b7790c26df82f24e
harshpa324 <harshpa324@gmail.com>
2024-02-22 02:29:31 +0530

    updated cardlist

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -5,19 +5,30 @@ import Image from "next/image";
 import Card from "@/components/card/card";
 
 const getData = async (page, cat) => {
-  const res = await fetch(
+  try{
+     const res = await fetch(
     `https://blogify-rosy.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
-    {
+     {
       cache: "no-store",
-    }
-  );
+     }
+   );
 
-  if (!res.ok) {
+   if (!res.ok) {
     throw new Error("Failed");
-  }
+   }
 
   return res.json();
-};
+} catch (e) {
+  if (e instanceof AggregateError) {
+    console.error("Aggregate error occurred:", e);
+    // Handle the aggregate error appropriately
+    console.error(e.errors); // Log individual errors aggregated in the AggregateError
+  } else {
+    console.error("Other error occurred:", e);
+    // Handle other types of errors
+  }
+}
+} ;
 
 const CardList = async ({ page, cat }) => {
   const { posts, count } = await getData(page, cat);
