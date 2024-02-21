de06786da79e0a462bc218f7358b770e96a32872
harshpa324 <harshpa324@gmail.com>
2024-02-22 00:03:45 +0530

    finished write

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -5,7 +5,7 @@ import Comments from "@/components/comments/Comments";
 
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`https://main--blogify1.netlify.app//api/posts/${slug}`, {
     cache: "no-store",
   });
 
