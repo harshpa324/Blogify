fcb55fb6c79a35cd2c4e7659003d583f69c2a1e2
harshpa324 <harshpa324@gmail.com>
2024-02-22 16:33:07 +0530

    updated cardlist

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -6,7 +6,7 @@ import Card from "@/components/card/card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    `https://blogify-rosy.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
