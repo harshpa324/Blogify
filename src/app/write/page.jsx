384264b608bf11be5575c3308f2bd31c72fd53b9
harshpa324 <harshpa324@gmail.com>
2024-02-26 20:22:32 +0530

    test1

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -79,7 +79,7 @@ const WritePage = () => {
       .replace(/^-+|-+$/g, "");
 
   const handleSubmit = async () => {
-    const res = await fetch("https://blogify1.netlify.app/api/posts", {
+    const res = await fetch("/api/posts", {
       method: "POST",
       body: JSON.stringify({
         title,
